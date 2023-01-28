3f46705d93dbbc2ba1c486d9df060cf329449d6a
Aditya <chaudharyaditya.in@gmail.com>
2023-01-28 13:24:30 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>
+          Krishna Engineering 
+        </h1>
+        <h6>
+          EOT cranes Manufacturer EOT Cranes, Electric Hoist, Heavy Lifting Machines, Goods Lifting Machines Manufacturer. Own Manufacturing Facility, Please Visit us for Detailed Quotes for your Industrial Requirements
+        </h6>
+        <a
+          href="https://goo.gl/maps/o9ZQiHMygv7YHgz26"
+        >
+         Location 
+        </a>
+        <h3>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14055.212584124165!2d77.274764!3d28.2739853!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cd9803d8b80f7%3A0x2faa26618bec3999!2sKrishna%20Engineering!5e0!3m2!1sen!2sin!4v1674891196302!5m2!1sen!2sin" width="600" height="450"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </h3>
+    </div>
+  );
+}
+
+export default App;
